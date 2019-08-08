5752d6e0d0f5989512393b4b0de48eb5d86df53e
u17 <lewpanu@gmail.com>
2019-08-08 17:15:08 +0700

    TrainML Issues

---
+++ IssuePrediction.cs
@@ -0,0 +1,8 @@
+using Microsoft.ML.Data;
+namespace issuesML{
+    public class IssuePrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public string Area;
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+##Train Isuues
+
+
+```bash
+
+```
